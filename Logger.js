fcb498e46e148088a5df60f5542c1fd4e19fc71d
dadams <adams.t.david@gmail.com>
2019-06-16 10:12:06 -0600

    add tets for Logger along with refactors to Logger from writing tests

--- Logger.js
+++ Logger.js
@@ -18,21 +18,31 @@ class Logger {
   addAction(actionJSON) {
     return new Promise((resolve, reject) => {
       //setTimeout is used here to simulate async activity
-
       setTimeout(() => {
         try {
+          if (!actionJSON) {
+            throw new Error('There was no action provided.');
+          }
+
           const { action: actionName, time } = JSON.parse(actionJSON);
+
+          if (
+            !actionName ||
+            actionName.length === 0 ||
+            typeof time !== 'number'
+          ) {
+            throw new Error('There was an error with the provided action.');
+          }
 
           if (this.actions.hasOwnProperty(actionName)) {
             // If the action already exists, then we need to update the avg and count for the action
-
             const action = this.actions[actionName];
 
+            action.avg =
+              (action.avg * action.count + time) / (action.count + 1);
             action.count += 1;
-            action.avg = (action.avg + time) / action.count;
           } else {
             // If the action doesn't already exist, we need to create a new entry for it
-
             this.actions[actionName] = {
               avg: time,
               count: 1,
@@ -41,7 +51,10 @@ class Logger {
 
           resolve();
         } catch (error) {
-          reject(error);
+          reject({
+            error: error.toString(),
+            status: 'error',
+          });
         }
       });
     });
@@ -49,7 +62,7 @@ class Logger {
 
   /**
    * Retrieves all of the stats for all of the actions that have been logged
-   * @return {Promise} - Returns a promise that resolves with the list of actions and their stats
+   * @return {Promise} - Returns a promise that resolves with a JSON serialized list of actions and their stats
    */
   getStats() {
     return new Promise((resolve, reject) => {
@@ -59,21 +72,24 @@ class Logger {
            * We need to turn the actions map into an array of objects where
            * each object contains information about the action.
            */
-
           const stats = Object.keys(this.actions).reduce((acc, actionKey) => {
+            const { avg } = this.actions[actionKey];
+            const roundedAvg = Number(Math.round(avg + 'e2') + 'e-2');
+
             acc.push({
               action: actionKey,
-              avg: this.actions[actionKey].avg,
+              avg: roundedAvg,
             });
 
             return acc;
           }, []);
 
-          const statsJSON = JSON.stringify(stats);
-
-          resolve(statsJSON);
+          resolve(JSON.stringify(stats));
         } catch (error) {
-          reject(error);
+          reject({
+            error: error.toString(),
+            status: 'error',
+          });
         }
       });
     });
---
+++ Logger.test.js
@@ -0,0 +1,151 @@
+const Logger = require('./Logger');
+
+describe('Logger', () => {
+  let logger;
+
+  beforeEach(() => {
+    logger = new Logger();
+  });
+
+  describe('when adding an action', () => {
+    describe('when the action is invalid JSON', () => {
+      test('it should reject the promise with an error', async () => {
+        expect.assertions(1);
+
+        try {
+          await logger.addAction('invalid json string');
+        } catch (error) {
+          expect(error.error.startsWith('SyntaxError:')).toBe(true);
+        }
+      });
+    });
+
+    describe('when the action does not exist', () => {
+      test('should reject the promise with an error', async () => {
+        expect.assertions(1);
+
+        try {
+          await logger.addAction();
+        } catch (error) {
+          expect(error.error).toBe('Error: There was no action provided.');
+        }
+      });
+    });
+
+    describe('when the action is valid json', () => {
+      describe('and the action is null', () => {
+        test('should reject the promise with an error', async () => {
+          expect.assertions(1);
+
+          try {
+            await logger.addAction(null);
+          } catch (error) {
+            expect(error.error).toBe('Error: There was no action provided.');
+          }
+        });
+      });
+
+      test('should reject the promise with an error when the action name is null', async () => {
+        expect.assertions(1);
+
+        try {
+          await logger.addAction('{"action":null, "time":75}');
+        } catch (error) {
+          expect(error.error).toBe(
+            'Error: There was an error with the provided action.'
+          );
+        }
+      });
+
+      test('should reject the promise with an error when the action name is empty', async () => {
+        expect.assertions(1);
+
+        try {
+          await logger.addAction('{"action":"", "time":75}');
+        } catch (error) {
+          expect(error.error).toBe(
+            'Error: There was an error with the provided action.'
+          );
+        }
+      });
+
+      describe('when the action time is not a number', () => {
+        test('should reject the promise with an error', async () => {
+          expect.assertions(1);
+
+          try {
+            await logger.addAction('{"action":"", "time":"75"}');
+          } catch (error) {
+            expect(error.error).toBe(
+              'Error: There was an error with the provided action.'
+            );
+          }
+        });
+      });
+    });
+  });
+
+  describe('when retrieving all stats', () => {
+    describe('when no actions have been added', () => {
+      test('should resolve the promise with an empty JSON list of actions', async () => {
+        const actions = await logger.getStats();
+
+        expect(actions).toBe('[]');
+      });
+    });
+
+    describe('when only one action has been added', () => {
+      test('should resolve the promise with a JSON list of the action and its stats', async () => {
+        await logger.addAction('{"action":"action1", "time":50}');
+        const actions = await logger.getStats();
+
+        expect(actions).toBe('[{"action":"action1","avg":50}]');
+      });
+    });
+
+    describe('when multiple actions have been added', () => {
+      test('should resolve the promise with a JSON list of the actions and their stats', async () => {
+        await logger.addAction('{"action":"action1", "time":50}');
+        await logger.addAction('{"action":"action1", "time":100}');
+        await logger.addAction('{"action":"action1", "time":150}');
+        await logger.addAction('{"action":"action1", "time":200}');
+        const actions = await logger.getStats();
+
+        expect(actions).toBe('[{"action":"action1","avg":125}]');
+      });
+    });
+
+    describe('when multiple actions with multiple entries have been added', () => {
+      test('should resolve the promise with a JSON list of the actions and their stats', async () => {
+        await logger.addAction('{"action":"action1", "time":5}');
+        await logger.addAction('{"action":"action2", "time":20}');
+        await logger.addAction('{"action":"action3", "time":30}');
+        await logger.addAction('{"action":"action3", "time":40}');
+        await logger.addAction('{"action":"action2", "time":50}');
+        await logger.addAction('{"action":"action1", "time":10}');
+        await logger.addAction('{"action":"action2", "time":70}');
+        await logger.addAction('{"action":"action1", "time":15}');
+        await logger.addAction('{"action":"action3", "time":90}');
+        await logger.addAction('{"action":"action1", "time":10}');
+        const actions = await logger.getStats();
+
+        expect(actions).toBe(
+          '[{"action":"action1","avg":10},{"action":"action2","avg":46.67},{"action":"action3","avg":53.33}]'
+        );
+      });
+    });
+
+    describe('when there is an error', () => {
+      test('should reject the promise with an error', async () => {
+        expect.assertions(1);
+
+        try {
+          logger.actions = null;
+          await logger.getStats();
+        } catch (error) {
+          expect(error.status).toBe('error');
+        }
+      });
+    });
+  });
+});
